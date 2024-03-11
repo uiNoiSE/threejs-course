157feb4349cef287734e0581e4a0f18d7ddc2852
uiNoiSE <uiNoiSE@gmail.com>
2024-03-11 16:21:06 +0300

    * fixed favicon and sharing for prod.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,16 @@
-<script setup>
+<script setup lang="ts">
+const config = useRuntimeConfig();
+
 useSeoMeta({
   title: 'Three.js Course',
   ogTitle: `uiNoiSE's Journey in Mastering Three.js: Unveiling the Power of 3D Web Graphics`,
   description: `Explore my journey in learning Three.js, a powerful JavaScript library for creating 3D graphics. Each lesson is presented on a separate page, showcasing my progress and learnings along the way. Dive in to see how I'm mastering 3D web development!`,
   ogDescription: `Explore my journey in learning Three.js, a powerful JavaScript library for creating 3D graphics. Each lesson is presented on a separate page, showcasing my progress and learnings along the way. Dive in to see how I'm mastering 3D web development!`,
-  ogImage: '/sharing.jpg',
+  ogImage: `${config.app.baseURL}/sharing.jpg`,
   ogUrl: 'https://uinoise.github.io/threejs-course/',
   twitterTitle: `uiNoiSE's Journey in Mastering Three.js: Unveiling the Power of 3D Web Graphics`,
   twitterDescription: `Explore my journey in learning Three.js, a powerful JavaScript library for creating 3D graphics. Each lesson is presented on a separate page, showcasing my progress and learnings along the way. Dive in to see how I'm mastering 3D web development!`,
-  twitterImage: '/sharing.jpg',
+  twitterImage: `${config.app.baseURL}/sharing.jpg`,
   twitterCard: 'summary_large_image',
   formatDetection: 'telephone=no',
 });
@@ -18,13 +20,17 @@ useHead({
     lang: 'en',
   },
   link: [
-    { rel: 'shortcut icon', type: 'image/svg+xml', href: '/favicon.svg' },
+    {
+      rel: 'shortcut icon',
+      type: 'image/svg+xml',
+      href: `${config.app.baseURL}/favicon.svg`,
+    },
   ],
 });
 </script>
 
 <template>
-<!--  <header-block />-->
+  <!--  <header-block />-->
   <slot />
-<!--  <footer-block />-->
+  <!--  <footer-block />-->
 </template>
