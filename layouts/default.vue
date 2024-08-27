32628c27fcf9480d9fdb7210a0cba98a666cecfb
uiNoiSE <uiNoiSE@gmail.com>
2024-08-27 13:02:25 +0300

    fix(open-graph): fixed missing og:image.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-const config = useRuntimeConfig();
-
 useSeoMeta({
   title: 'Three.js Course',
   ogTitle: `uiNoiSE's Journey in Mastering Three.js: Unveiling the Power of 3D Web Graphics`,
   description: `Explore my journey in learning Three.js, a powerful JavaScript library for creating 3D graphics. Each lesson is presented on a separate page, showcasing my progress and learnings along the way. Dive in to see how I'm mastering 3D web development!`,
   ogDescription: `Explore my journey in learning Three.js, a powerful JavaScript library for creating 3D graphics. Each lesson is presented on a separate page, showcasing my progress and learnings along the way. Dive in to see how I'm mastering 3D web development!`,
-  ogImage: `${process.env.NODE_ENV === 'production' ? config.app.baseURL : ''}/sharing.jpg`,
+  ogImage: '/sharing.jpg',
   ogUrl: 'https://uinoise.github.io/threejs-course/',
   twitterTitle: `uiNoiSE's Journey in Mastering Three.js: Unveiling the Power of 3D Web Graphics`,
   twitterDescription: `Explore my journey in learning Three.js, a powerful JavaScript library for creating 3D graphics. Each lesson is presented on a separate page, showcasing my progress and learnings along the way. Dive in to see how I'm mastering 3D web development!`,
-  twitterImage: `${process.env.NODE_ENV === 'production' ? config.app.baseURL : ''}/sharing.jpg`,
+  twitterImage: '/sharing.jpg',
   twitterCard: 'summary_large_image',
   formatDetection: 'telephone=no',
 });
@@ -26,7 +24,7 @@ useHead({
   }, {
     rel: 'shortcut icon',
     type: 'image/svg+xml',
-    href: `${process.env.NODE_ENV === 'production' ? config.app.baseURL : ''}/favicon.svg`,
+    href: '/favicon.svg',
   }],
 });
 </script>
